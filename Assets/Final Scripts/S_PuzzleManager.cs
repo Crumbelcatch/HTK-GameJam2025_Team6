5fe1eec6e1cb33dda8e9624479287e73a73c05d6
Crumbelcatch <maltelantow@protonmail.com>
2025-06-13 22:35:34 +0200

    FMOD Switch & Drag, Drop, yes, no & Transition Sounds

--- Assets/Final Scripts/S_PuzzleManager.cs
+++ Assets/Final Scripts/S_PuzzleManager.cs
@@ -1,15 +1,37 @@
+using FMODUnity;
+using FMOD.Studio;
+
 using System.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class S_PuzzleManager : MonoBehaviour
 {
+
+    FMOD.Studio.EventInstance _completedEvent;
+
     public GameObject[] Puzzles;
     public int puzzleCount;
+
+    [Header("Audio Settings")]
+    [SerializeField] public FMODUnity.EventReference _completedSound;
 
     private void Awake()
     {
         puzzleCount = 0;
+    }
+
+    private void Update()
+    {
+        if (_completedEvent.isValid())
+        {
+            FMOD.Studio.PLAYBACK_STATE playbackState;
+            _completedEvent.getPlaybackState(out playbackState);
+            if (playbackState == FMOD.Studio.PLAYBACK_STATE.STOPPED)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+            }
+        }
     }
 
     public void SolvedCheck()
@@ -24,13 +46,18 @@ public class S_PuzzleManager : MonoBehaviour
         int arrayLength = Puzzles.Count();
         if (puzzleCount == arrayLength) 
         {
-            Debug.Log("Puzzle solved. Now leave.");
+            //Debug.Log("Puzzle solved. Now leave.");
             //Puzzle Solved! Go next goes here ->
             //Unlock next Level in Level Select
             UnlockLevel();
 
+
+            _completedEvent = FMODUnity.RuntimeManager.CreateInstance(_completedSound);
+
+            _completedEvent.start();
+
             //Load next Level
-            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+  
             return;
         }
         
@@ -41,6 +68,7 @@ public class S_PuzzleManager : MonoBehaviour
     {
         if (SceneManager.GetActiveScene().buildIndex >= PlayerPrefs.GetInt("ReachedIndex"))
         {
+            FMODUnity.RuntimeManager.PlayOneShot(_completedSound, transform.position);
             PlayerPrefs.SetInt("ReachedIndex", SceneManager.GetActiveScene().buildIndex + 1);
             PlayerPrefs.GetInt("UnlockedIndex", PlayerPrefs.GetInt("UnlockedLevel") + 1);
             PlayerPrefs.Save();
